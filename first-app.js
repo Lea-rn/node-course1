854ad3dcce8143b05fd32ab6f5d365b272be2b96
achrefmechergui <achreffcb123achref@gamil.com>
2026-03-23 22:03:57 +0100

    course1

---
+++ first-app.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+
+// fs.writeFileSync("hello.txt", "hello from Node.js");
+
+const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   //   console.log(req.url, req.method, req.headers);
+//   res.setHeader("Content-type", "text/html");
+//   res.write("<html>");
+//   res.write("<head> <title> First Page </title> </head>");
+//   res.write("<body> <h1> hello from Node.js server !!! </h1> </body>");
+
+//   res.write("</html>");
+//   res.end();
+// });
+
+// server.listen(3000);
+
+////
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head> <title> Enter Message </title> </head>");
+    res.write(
+      '<body> <form action="/message" method="POST" > <input type="text" name="message" /> <button type="submit"> send</button> </form> </body>',
+    );
+
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedData = Buffer.concat(body).toString();
+      console.log(parsedData);
+      const message = parsedData.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("location", "/"); //// temporary redirection
+    return res.end();
+  }
+});
+
+server.listen(4000);
